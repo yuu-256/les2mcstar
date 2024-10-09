509cf8c252a9e765439356ce9ff24cbdea1c6c96
yuu-256 <ezayuu12@icloud.com>
2024-10-09 18:29:13 +0900

    driver/get volume concentration

--- module/libgen.py
+++ module/libgen.py
@@ -10,3 +10,20 @@ def generate_random_coeff(rows, cols):
     formatted_text = "\n".join(" ".join(f"{value:.1f}" for value in row) for row in random_array)
     
     return formatted_text
+
+### TO CONVERT ARRAY TO TEXT INPUT ###
+def array_to_input(array):
+    lines = []
+    ### ITERATING OVER ROWS ###
+    for row in array:
+        ### FORMATTING ROW ###
+        formatted_row = '  '.join(f'{val:.1E}' for val in row)
+        lines.append(formatted_row)
+    ### JOINING ALL ROWS ###
+    text_output = '\n'.join(lines)
+    return text_output
+
+def save_to_file(text, filename):
+    with open(filename, 'w') as file:
+        file.write(text)
+    return
