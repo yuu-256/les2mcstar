77ba66ea15e09a17b267a47e39368dc9a990e946
yuu-256 <ezayuu12@icloud.com>
2024-10-13 22:44:10 +0900

    make input text

--- module/libgen.py
+++ module/libgen.py
@@ -23,7 +23,27 @@ def array_to_input(array):
     text_output = '\n'.join(lines)
     return text_output
 
+### TO SAVE TEXT TO FILE ###
 def save_to_file(text, filename):
     with open(filename, 'w') as file:
         file.write(text)
+    return
+
+### COMBINE TEXT FILES TO INOUT FORMAT###
+def write_combined_text_files(outpath, imax):
+    with open(outpath, 'w') as outfile:
+        for i in range(0, imax + 1):
+            ### FILENAME ###
+            filename = f'../data/volume_concentration_{i}.txt'
+            try:
+                with open(filename, 'r') as infile:
+                    content = infile.read()
+                    
+                    # 書き出すテキストのフォーマット
+                    outfile.write(f'1 {i+1}  ipoly lz/cconc(i,j,lz,ipoly)\n')
+                    outfile.write(content)
+                    outfile.write(f'1 {i+1+1}  ipoly lz/cconc(i,j,lz,ipoly)\n')
+            except FileNotFoundError:
+                print(f"{filename} not found. Skipping...")
+                continue  
     return
